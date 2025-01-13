f98f284c4cd0b65b963093eb1e73b4579ce660d1
Okinea Dev <hi@okinea.dev>
2025-01-13 18:38:19 +0200

    fix: fix action

--- add-or-remove-labels.sh
+++ add-or-remove-labels.sh
@@ -15,18 +15,15 @@ if [[ ! -n "$LABELS" ]]; then
   exit 1
 fi
 
-# Add each label using gh cli
-command="$(
-  gh issue edit "$ISSUE_NUMBER" --$ACTION-label "$LABELS"
-)"
+# Add labels using gh cli
+gh issue edit "$ISSUE_NUMBER" --$ACTION-label "$LABELS"
 
-if [[ "$command" -eq 0 ]]; then
-  [[ $ACTION == "add" ]] && echo "✅ Added labels \"$label\" for #$ISSUE_NUMBER"
-  [[ $ACTION == "remove" ]] && echo "✅ Removed labels \"$label\" from #$ISSUE_NUMBER"
+if [[ $? -eq 0 ]]; then
+  [[ $ACTION == "add" ]] && echo "✅ Added labels \"$LABELS\" for #$ISSUE_NUMBER"
+  [[ $ACTION == "remove" ]] && echo "✅ Removed labels \"$LABELS\" from #$ISSUE_NUMBER"
 else
-  echo "Error: Failed to $ACTION label $label"
-  [[ $ACTION == "add" ]] && echo "Error: Failed to add labels \"$label\" for #$ISSUE_NUMBER"
-  [[ $ACTION == "remove" ]] && echo "Error: Failed to remove labels \"$label\" from #$ISSUE_NUMBER"
+  [[ $ACTION == "add" ]] && echo "Error: Failed to add labels \"$LABELS\" for #$ISSUE_NUMBER"
+  [[ $ACTION == "remove" ]] && echo "Error: Failed to remove labels \"$LABELS\" from #$ISSUE_NUMBER"
   exit 1
 fi
 
