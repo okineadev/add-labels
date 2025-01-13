6759c4f0c08b3b5cbf53445ea025a1c17806681c
Okinea Dev <hi@okinea.dev>
2025-01-13 18:31:14 +0200

    fix: fix action

--- add-or-remove-labels.sh
+++ add-or-remove-labels.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -z "$GITHUB_TOKEN" ]; then
+if [[ ! -n "$GITHUB_TOKEN" ]]; then
   echo "Error: GITHUB_TOKEN is not set"
   exit 1
 fi
@@ -10,14 +10,14 @@ ACTION="$2"
 
 # Read labels from input
 LABELS="$3"
-if [ ! -n "$LABELS" ]; then
+if [[ ! -n "$LABELS" ]]; then
   echo "Error: No labels provided"
   exit 1
 fi
 
 # Add each label using gh cli
 command="$(
-  gh issue edit "$ISSUE_NUMBER" --$ACTION-label "$label"
+  gh issue edit "$ISSUE_NUMBER" --$ACTION-label "$LABELS"
 )"
 
 if [[ "$command" -eq 0 ]]; then
@@ -27,7 +27,6 @@ else
   echo "Error: Failed to $ACTION label $label"
   [[ $ACTION == "add" ]] && echo "Error: Failed to add labels \"$label\" for #$ISSUE_NUMBER"
   [[ $ACTION == "remove" ]] && echo "Error: Failed to remove labels \"$label\" from #$ISSUE_NUMBER"
-  echo "$command"
   exit 1
 fi
 
